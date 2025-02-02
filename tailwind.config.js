9d9a7a20f8d3f2e5e967f98a6ba389f7f797a847
munyimJR <muntasirpolash@gmail.com>
2025-02-02 16:29:45 +0600

    15th commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
           borderRadius: theme('borderRadius.lg'),
           padding: theme('spacing.6'),
           boxShadow: theme('boxShadow.xl'),
-          borderColor: theme('colors.orange.500'), // Corrected to use Tailwind color for orange
-          borderWidth: '1px', // Added border width for visibility
+          borderColor: theme('colors.orange.500'),
+          borderWidth: '1px',
         }
       })
     })
